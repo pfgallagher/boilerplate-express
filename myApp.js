f6f1c58be9defaf6da45aa37ba8cfa560f5375b7
pfgallagher <pfgallagher@gmail.com>
2019-04-14 15:17:39 -0500

    Add another static route

--- myApp.js
+++ myApp.js
@@ -9,6 +9,7 @@ const path = require("path");
 console.log("Hello World");
 
 app.use(express.static(path.join(__dirname, "./views")));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.get("/", (req, res, next) => {
 	res.send("Hello Express");
